453c388547b095b8671a3648a6d6fbb109a9afad
felipebuso <felipe.buso@hotmail.com>
2025-03-17 06:51:16 -0300

    feat: java-doc

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,16 @@
+/**
+ * @author Felipe Buso
+ * @version 1.0
+ * @since 17/03/2025
+ */
+
+public class Calculadora {
+    /**
+     * @param numeroUm   primeiro parametro
+     * @param numeroDois segundo parametr
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
